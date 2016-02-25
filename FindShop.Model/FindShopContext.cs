55d5d98b13cfa577c7310f1db47d900c58ddeac5
Xavier Er <xavier_er@live.com.sg>
2016-02-25 23:39:58 +0800

    Add FindShopContext

--- FindShop.Model/FindShopContext.cs
+++ FindShop.Model/FindShopContext.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FindShop.Model
 {
-    public class FindShopContext
+    public class FindShopContext : DbContext
     {
+        public FindShopContext() : this("FindShopContext")
+        {
+        }
+
+        public FindShopContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+        }
     }
 }
