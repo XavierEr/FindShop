f2548815dd03d4def2ec1dad0c663d52f23bdf52
Xavier Er <xavier_er@live.com.sg>
2016-02-26 18:20:29 +0800

    Add ShopsController and AddShopRequestViewModel

---
+++ FindShop.Api/Controllers/ShopsController.cs
@@ -0,0 +1,54 @@
+﻿using FindShop.Api.ViewModels;
+using FindShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace FindShop.Api.Controllers
+{
+    public class ShopsController : ApiController
+    {
+        // GET: api/Shops
+        public async Task<IEnumerable<Shop>> Get()
+        {
+            using (var findShopContext = new FindShopContext())
+            {
+                return await findShopContext.Shops.ToArrayAsync();
+            }
+        }
+
+        // GET: api/Shops/5
+        public async Task<Shop> Get(Guid id)
+        {
+            using (var findShopContext = new FindShopContext())
+            {
+                return await findShopContext.Shops.SingleAsync(i => i.Id == id);
+            }
+        }
+
+        // POST: api/Shops
+        public async Task Post([FromBody]AddShopRequestViewModel addShopRequestViewModel)
+        {
+            using (var findShopContext = new FindShopContext())
+            {
+                findShopContext.Shops.Add(new Shop() { Name = addShopRequestViewModel.Name });
+
+                await findShopContext.SaveChangesAsync();
+            }
+        }
+
+        // DELETE: api/Shops/5
+        public async Task Delete(Guid id)
+        {
+            using (var findShopContext = new FindShopContext())
+            {
+                await findShopContext.Database.ExecuteSqlCommandAsync("DELETE FROM Shops WHERE Id = @p0", id);
+            }
+        }
+    }
+}
---
+++ FindShop.Api/ViewModels/AddShopRequestViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindShop.Api.ViewModels
+{
+    public class AddShopRequestViewModel
+    {
+        public string Name { get; set; }
+    }
+}
