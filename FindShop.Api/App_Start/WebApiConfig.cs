1a031bcc566c8c56cf406693955ca924ca641b4f
Xavier Er <xavier_er@live.com.sg>
2016-02-26 15:09:54 +0800

    Set json format to be in camel case

--- FindShop.Api/App_Start/WebApiConfig.cs
+++ FindShop.Api/App_Start/WebApiConfig.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json.Serialization;
 using System.Web.Http;
 
 namespace FindShop.Api
@@ -10,6 +8,8 @@ namespace FindShop.Api
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var jsonFormatter = config.Formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
